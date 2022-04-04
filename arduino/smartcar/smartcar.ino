6ae0344adb9ffac20522285ec3b070dcc0f841b7
Sannazak <sanna.zakrisson@live.com>
2022-04-04 18:13:51 +0200

    Changes by harri

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,10 +1,29 @@
 #include <Smartcar.h>
 
+const int forwardSpeed = 70; // 70% of the full speed forward
+const int reverseSpeed   = -70; // 70% of the full speed backward
+const int leftDegrees = -75; // degrees to turn left
+const int rightDegrees = 75;  // degrees to turn right
+
+
+
+// Copied from Dimitrios blog
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+SimpleCar car(control);
+
 void setup() {
   // put your setup code here, to run once:
+    Serial.begin(9600); //starts and waits for some time to sensors to start
 }
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(1);
+  handleInput();
+
 }
 
 void handleInput()
