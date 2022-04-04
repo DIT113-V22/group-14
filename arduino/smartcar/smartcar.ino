afcdab11ac6dc824804259f01c2e02ce8ef44b7e
Sannazak <sanna.zakrisson@live.com>
2022-04-04 18:13:26 +0200

    issue #6 Everyone coded

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -7,3 +7,34 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+void handleInput()
+{
+    if(Serial.available())
+    {
+        char input = Serial.read();
+
+        switch (input)
+        {
+            case 'l':
+                car.setSpeed(forwardSpeed);
+                car.setAngle(leftDegrees);
+                break;
+            case 'r':
+                car.setSpeed(forwardSpeed);
+                car.setAngle(rightDegrees);
+                break;
+            case 'f':
+                car.setSpeed(forwardSpeed);
+                car.setAngle(0);
+                break;
+            case 'b':
+                car.setSpeed(reverseSpeed);
+                car.setAngle(0);
+                break;
+            default:
+                car.setSpeed(0);
+                car.setAngle(0);
+        }
+    }
+}
