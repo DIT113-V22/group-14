f6f3b5aad1e764ce18235ff033b55c30a48a6c7e
Gabriela Istrate <gistrate09@gmail.com>
2022-04-13 13:42:17 +0200

    Add custom map assets
    Added ground, hedges, plant assets. Added collider to the ground.

--- GODOT/smcegd_mod_example/src/environments/example/Example.gd
+++ GODOT/smcegd_mod_example/src/environments/example/Example.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 export var max_balls: int = 20
 
-func _ready() -> void:
+"""func _ready() -> void:
 	var timer: Timer = Timer.new()
 	timer.wait_time = 2
 	add_child(timer)
@@ -10,11 +10,11 @@ func _ready() -> void:
 	timer.start()
 
 var balls: Array = []
-
-func _on_timeout() -> void:
+"""
+"""func _on_timeout() -> void:
 	var ball: RigidBody = preload("res://src/environments/example/square.tscn").instance()
 	add_child(ball)
-	
+
 	var rand_vec: Vector3 = Vector3(rand_range(0,10),rand_range(0,10),rand_range(0,10))
 	ball.apply_central_impulse(rand_vec)
 	ball.apply_torque_impulse(rand_vec)
@@ -22,17 +22,14 @@ func _on_timeout() -> void:
 	balls.push_back(ball)
 	if balls.size() > max_balls:
 		balls.pop_front().queue_free()
-
+"""
 
 func init_cam_pos() -> Basis:
 	return $Camera.global_transform
 
 func get_spawn_position(hint: String) -> Transform:
-	var gt = global_transform
+	match hint:
+		"debug_vehicle": return $DebugVehicleSpawn.global_transform
+		_: return $VehicleSpawn.global_transform
 	
-	gt.origin.y += 3.0
-	
-	gt.origin.x += rand_range(-8.0, 8.0)
-	gt.origin.z += rand_range(-8.0, 8.0)
-	
-	return gt
+
