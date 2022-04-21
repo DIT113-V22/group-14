fd0868528714631172e9c5bf81ae3feab2a73db0
Erik Harring <harring_@hotmail.com>
2022-04-21 13:39:03 +0200

    updated scipt/map name
    updated file names and changed the file paths in scripts to reflect

--- GODOT/smcegd_mod_example/mods/example_mod.gd
+++ GODOT/smcegd_mod_example/mods/example_mod.gd
@@ -3,6 +3,6 @@ extends Reference
 var mod_name: String = "example"
 
 func init(global) -> void:
-	global.register_environment("example/Example", load("res://src/environments/example/Example.tscn"))
+	global.register_environment("plant_garden/main_plant_garden", load("res://src/environments/plant_garden/main_plant_garden.tscn"))
 	global.register_vehicle("StupidCar", load("res://src/vehicles/stupid_car/StupidCar.tscn"))
 	print("Hello World!")
