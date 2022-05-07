c5b0a4e134638e5aed9a6b3142255aad7da07ecc
Erik Harring <harring_@hotmail.com>
2022-05-07 18:30:20 +0200

    added popup message
    Added poppup message when email and/or password field is empty

--- android/SmartcarMqttController/app/src/main/java/platis/solutions/smartcarmqttcontroller/LoginScreen.java
+++ android/SmartcarMqttController/app/src/main/java/platis/solutions/smartcarmqttcontroller/LoginScreen.java
@@ -7,6 +7,8 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
 
 public class LoginScreen extends AppCompatActivity {
     @Override
@@ -19,14 +21,27 @@ public class LoginScreen extends AppCompatActivity {
                 android.view.WindowInsets.Type.statusBars()
         );
 
+
+
         Button login = findViewById(R.id.login);
+        EditText emailText = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        EditText passwordText = (EditText) findViewById(R.id.editTextTextPassword);
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginScreen.this, HomeScreen.class);
-                startActivity(intent);
+                if (emailText.getText().toString().isEmpty() || passwordText.getText().toString().isEmpty()) {
+                    startActivity(new Intent(LoginScreen.this,Pop.class));
+                } else {
+                    Intent intent = new Intent(LoginScreen.this, HomeScreen.class);
+                    startActivity(intent);
+                }
+
             }
         });
+
 
         Button exit = findViewById(R.id.exit);
         exit.setOnClickListener(new View.OnClickListener() {
---
+++ android/SmartcarMqttController/app/src/main/java/platis/solutions/smartcarmqttcontroller/Pop.java
@@ -0,0 +1,22 @@
+package platis.solutions.smartcarmqttcontroller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class Pop extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popwindow);
+        DisplayMetrics displayMetricsVar = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetricsVar);
+
+        int width = displayMetricsVar.widthPixels;
+        int height = displayMetricsVar.heightPixels;
+
+        getWindow().setLayout((int)(width*.4), (int)(height*.35));
+    }
+}
