e9ba6149190f91f435ddcac12045d04d2ed6d32f
Erik Harring <harring_@hotmail.com>
2022-05-11 15:34:20 +0200

    added database integration
    Co-Authored-By: Sanna Zakrisson <71096010+Sannazak@users.noreply.github.com>

--- android/PlantholderApplication/app/src/main/java/plantholder/application/Firebase.java
+++ android/PlantholderApplication/app/src/main/java/plantholder/application/Firebase.java
@@ -1,29 +1,20 @@
 package plantholder.application;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Firebase extends AppCompatActivity {
 
     private DatabaseReference myDatabase;
+    private DatabaseReference myPlants;
 
 
 
@@ -48,64 +39,6 @@ public class Firebase extends AppCompatActivity {
         myDatabase = database.getReference("Plants");
     }
 
-    public void getPlantInformation(String ID){
-
-       // DatabaseReference database = FirebaseDatabase.getInstance("https://planty-9e09f-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Plants");
-DatabaseReference database = FirebaseDatabase.getInstance().getReference("Plants");
-        Query getType = myDatabase.orderByChild("id").equalTo(ID);
-        Query getType = myDatabase()
-        //Query query=users.child(auth.getCurrentUser().getUid()).orderByChild("email");
-        getType.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    String plantType = snapshot.child(ID).child("species").getValue(String.class);
-                    String plantStatus = snapshot.child(ID).child("health").getValue(String.class);
-                    int plantRow= snapshot.child(ID).child("row").getValue(int.class);
-                    int plantColumn = snapshot.child(ID).child("column").getValue(int.class);
-
-
-                    /*Intent intent = new Intent(getApplicationContext(),Plants.class);;
-                    intent.putExtra("ID", ID);
-                    intent.putExtra("species", plantType);
-                    intent.putExtra("row", plantRow);
-                    intent.putExtra("column", plantColumn);
-                    intent.putExtra("health", plantStatus);
-                    TextView plantIdText = (TextView)findViewById(R.id.plantIdTextView);
-                    plantIdText.setText("ID: " + plantStatus);
-
-                    startActivity(intent);
-
-                     */
-
-
-                    TextView plantTypeText = (TextView)findViewById(R.id.plantTypeTextView);
-                    plantTypeText.setText(String.format("Type: %s", plantType));
-
-                    TextView plantStatusText = (TextView)findViewById(R.id.plantStatusTextView);
-                    plantStatusText.setText(String.format("Status: %s", plantStatus));
-
-                    TextView plantRowText = (TextView)findViewById(R.id.plantRowTextView);
-                    plantRowText.setText(String.format("Row: %s", plantRow));
-
-                    TextView plantColumnText = (TextView)findViewById(R.id.plantColumnTextView);
-                    plantColumnText.setText(String.format("Column: %s", plantColumn));
-
-                } else {
-                    TextView plantIdText = (TextView)findViewById(R.id.plantIdTextView);
-                    String noSuchThing = "ID does not exist";
-                    plantIdText.setText(noSuchThing);
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-    }
 
     // Do not delete method, we will use it later
     public void writeNewPlant(String ID,String species, int row, int column, String health){
--- android/PlantholderApplication/app/src/main/java/plantholder/application/StatusScreen.java
+++ android/PlantholderApplication/app/src/main/java/plantholder/application/StatusScreen.java
@@ -1,15 +1,17 @@
 package plantholder.application;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,51 +19,58 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
+
+
 
 public class StatusScreen extends AppCompatActivity {
 
-    private Firebase firebase;
+    Firebase firebase;
 
-    String scannedPlantId = "1846973667";
-    String scannedTypeText = "bad query";
-    String scannedStatusText = "bad query";
-    String scannedRowText = "Row: x";
-    String scannedColumnText = "Column: x";
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference firebaseReference;
 
+
+    String scannedPlantId = "1846973667"; //placeholder value for the qr code
+
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_screen);
-        firebase = new Firebase();
-
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //set id text
+        TextView textViewID = findViewById(R.id.plantIdTextView);
+        textViewID.setText(scannedPlantId);
+
+        //Set health text
+        firebaseDatabase =  FirebaseDatabase.getInstance();
+        firebaseReference = firebaseDatabase.getReference("Plants").child("1846973667").child("health");
+        TextView textViewHealth = findViewById(R.id.plantStatusTextView);
+        getdata(textViewHealth);
+
+        //set type text
+        firebaseReference = firebaseDatabase.getReference("Plants").child("1846973667").child("species");
+        TextView textViewSpecies = findViewById(R.id.plantTypeTextView);
+        getdata(textViewSpecies);
+
+        //set row text
+        firebaseReference = firebaseDatabase.getReference("Plants").child("1846973667").child("row");
+        TextView textViewRow = findViewById(R.id.plantRowTextView);
+        getdataLong(textViewRow);
+
+        //set column text
+        firebaseReference = firebaseDatabase.getReference("Plants").child("1846973667").child("column");
+        TextView textViewColumn = findViewById(R.id.plantColumnTextView);
+        getdataLong(textViewColumn);
 
 
 
         getWindow().getDecorView().getWindowInsetsController().hide(
                 android.view.WindowInsets.Type.statusBars()
 );
-        TextView plantIdText = (TextView)findViewById(R.id.plantIdTextView);
-        plantIdText.setText(String.format("ID: %s", scannedPlantId));
-        firebase.getPlantInformation(scannedPlantId);
-/*
-        TextView plantIdText = (TextView)findViewById(R.id.plantIdTextView);
-        plantIdText.setText(String.format("ID: %s", scannedPlantId));
 
-        TextView plantTypeText = (TextView)findViewById(R.id.plantTypeTextView);
-        plantTypeText.setText(String.format("Type: %s", scannedTypeText));
-
-        TextView plantStatusText = (TextView)findViewById(R.id.plantStatusTextView);
-        plantStatusText.setText(String.format("Status: %s", scannedStatusText));
-
-        TextView plantRowText = (TextView)findViewById(R.id.plantRowTextView);
-        plantRowText.setText(String.format("Row: %s", scannedRowText));
-
-        TextView plantColumnText = (TextView)findViewById(R.id.plantColumnTextView);
-        plantColumnText.setText(String.format("Column: %s", scannedColumnText));
-
- */
 
 
 
@@ -104,6 +113,39 @@ public class StatusScreen extends AppCompatActivity {
             public void onClick(View view) {
                 firebase.updatePlantHealth("1846973667","Ripe");
             }
+        });
+
+    }
+
+    private void getdata(TextView textView) {
+        firebaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                textView.setText(value);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(StatusScreen.this, "Fail to get data", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+        //gets a textview and updates it with current references data
+    private void getdataLong(TextView textView) {
+        firebaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Long value = snapshot.getValue(Long.class);
+                String valueString = Long.toString(value);
+                textView.setText(valueString);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(StatusScreen.this, "Fail to get data", Toast.LENGTH_SHORT).show();
+            }
+
         });
 
     }
