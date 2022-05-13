6953dc65c1158e4136064bb12187f2f93266b49d
Sannazak <sanna.zakrisson@live.com>
2022-05-13 09:35:53 +0200

    try and add toast and searching the database for existing plant before adding new

--- android/PlantholderApplication/app/src/main/java/plantholder/application/AddPlantScreen.java
+++ android/PlantholderApplication/app/src/main/java/plantholder/application/AddPlantScreen.java
@@ -1,8 +1,8 @@
 package plantholder.application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +10,21 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-
-
-import android.widget.AdapterView;
 import android.widget.Toast;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddPlantScreen extends AppCompatActivity {
     private Firebase firebase;
+    private Plants plants;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference firebaseReference;
 
     private String selectedHealth;
     private String selectedType;
@@ -31,11 +39,15 @@ public class AddPlantScreen extends AppCompatActivity {
     private EditText editRow;
     private EditText editID;
 
+    private boolean plantExist;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_plant_screen);
         firebase = new Firebase();
+        plants = new Plants();
 
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
@@ -68,13 +80,42 @@ public class AddPlantScreen extends AppCompatActivity {
                 selectedHealth = spinnerPlantHealth.getSelectedItem().toString();
                 selectedType = spinnerPlantType.getSelectedItem().toString();
                 addedID = editID.getText().toString();
+
+                firebaseDatabase =  FirebaseDatabase.getInstance();
+                firebaseReference = firebaseDatabase.getReference("Plants").child(addedID);
+
+                getData();
+
                 addedColumn = Integer.parseInt(editColumn.getText().toString());
                 addedRow = Integer.parseInt(editRow.getText().toString());
 
-                firebase.writeNewPlant(addedID,selectedType,addedRow,addedColumn, selectedHealth);
+                if (addedID.length() < 10 ) {
+                    Toast.makeText(AddPlantScreen.this, "Plant ID needs to be 10 digits.", Toast.LENGTH_SHORT).show();
+                } else if (plantExist) {
+                    Toast.makeText(AddPlantScreen.this, "This plant ID already exist, you will update the existing plant.", Toast.LENGTH_LONG).show();
+                } else {
+                    firebase.writeNewPlant(addedID, selectedType, addedRow, addedColumn, selectedHealth);
+                    Toast.makeText(AddPlantScreen.this, "Plant created!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
 
-
+    private void getData() {
+        firebaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                if (value == null) {
+                    plantExist = false;
+                } else {
+                    plantExist = true;
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(AddPlantScreen.this, "Fail to get data", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     }
