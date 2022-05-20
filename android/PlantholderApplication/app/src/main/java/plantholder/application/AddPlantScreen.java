f1c2eca3b3c4595204732d8a976923a62482b38a
Erik Harring <harring_@hotmail.com>
2022-05-20 10:12:25 +0200

    fixed bug on checking if plant exists
    Co-Authored-By: Sanna Zakrisson <71096010+Sannazak@users.noreply.github.com>

--- android/PlantholderApplication/app/src/main/java/plantholder/application/AddPlantScreen.java
+++ android/PlantholderApplication/app/src/main/java/plantholder/application/AddPlantScreen.java
@@ -30,24 +30,17 @@ public class AddPlantScreen extends AppCompatActivity {
     private int addedRow;
     private int addedColumn;
 
-
     private Spinner spinnerPlantHealth;
     private Spinner spinnerPlantType;
     private EditText editColumn;
     private EditText editRow;
     private EditText editID;
 
-    private String plantValue;
-
-    private boolean plantExists;
-
     private Button back;
 
     public ArrayAdapter<CharSequence> adapter;
     public ArrayAdapter<CharSequence> adapter2;
 
-    DatabaseReference firebaseReference;
-    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +53,6 @@ public class AddPlantScreen extends AppCompatActivity {
         getWindow().getDecorView().getWindowInsetsController().hide(
                 android.view.WindowInsets.Type.statusBars()
         );
-
 
         back = (Button) findViewById(R.id.buttonBack);
         back.setOnClickListener(new View.OnClickListener() {
@@ -125,14 +117,8 @@ public class AddPlantScreen extends AppCompatActivity {
                     Toast.makeText(AddPlantScreen.this, "Please enter a column.", Toast.LENGTH_SHORT).show();
                 } else {
 
-                    verifyPlantExistence();
-                    if (plantValue != null) {
-                        idExistAlert();
-                    } else {
-                        createPlant();
-                        toastCreate();
-                    }
-                    plantValue = null;
+                    verifyPlantExistence(addedID);
+
                 }
             }
         });
@@ -180,29 +166,27 @@ public class AddPlantScreen extends AppCompatActivity {
 
     }
 
-    private void verifyPlantExistence() {
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseReference = firebaseDatabase.getReference("Plants").child(addedID).child("id");
-        firebaseReference.addValueEventListener(new ValueEventListener() {
-            boolean processDone = false;
+    private void verifyPlantExistence(String addedID) {
+        // Assuming the names are in the name node. Remove "name/" if not
+        DatabaseReference plantRef = FirebaseDatabase.getInstance().getReference("Plants/" + addedID);
+        plantRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (!processDone) {
-                    System.out.println(plantValue);
-                    plantValue = snapshot.getValue(String.class);
-                    System.out.println(plantValue);
-
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    idExistAlert();
+                } else {
+                    createPlant();
+                    toastCreate();
                 }
-                processDone = true;
-        }
+            }
 
-        @Override
-        public void onCancelled (@NonNull DatabaseError error){
-            Toast.makeText(AddPlantScreen.this, "Fail to get data", Toast.LENGTH_SHORT).show();
-        }
+            @Override
+            public void onCancelled(DatabaseError error) {
 
-    });
+            }
 
+        });
     }
 }
 
