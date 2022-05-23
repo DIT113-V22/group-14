148068f9ced1e1d22795292fe00f9ef95e73ed85
Gabriela Istrate <gistrate09@gmail.com>
2022-05-23 11:56:09 +0200

    change folder name and ignore .import

--- GODOT/smcegd_mod_example/mods/example_mod.gd
+++
@@ -1,8 +0,0 @@
-extends Reference
-
-var mod_name: String = "example"
-
-func init(global) -> void:
-	global.register_environment("plant_garden/main_plant_garden", load("res://src/environments/plant_garden/main_plant_garden.tscn"))
-	global.register_vehicle("StupidCar", load("res://src/vehicles/stupid_car/StupidCar.tscn"))
-	print("Hello World!")
